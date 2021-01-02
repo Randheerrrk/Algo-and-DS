9bf74246e6d26f7ff6818d813aa63bfd1f191848
randheerrrk <randheerrrk@gmail.com>
2021-01-02 16:04:08 +0530

    42 Trapping Rain Water

---
+++ Leetcode/42.java
@@ -0,0 +1,38 @@
+/*
+Trapping Rain Water
+-------------------
+
+Given n non-negative integers representing an elevation map where the width of each bar is 1, compute how much water it can trap after raining.
+
+Input: height = [0,1,0,2,1,0,1,3,2,1,2,1]
+Output: 6
+Explanation: The above elevation map (black section) is represented by array [0,1,0,2,1,0,1,3,2,1,2,1]. In this case, 6 units of rain water (blue section) are being trapped.
+
+Input: height = [4,2,0,3,2,5]
+Output: 9
+*/
+
+//Java Solution
+
+class Solution {
+    public int trap(int[] height) {
+        int n = height.length;
+        int[] left = new int[n];
+        int[] right = new int[n];
+        if(height.length==0) return 0;
+        left[0] = height[0];
+        right[n-1] = height[n-1];
+        for(int i=1; i<n; i++) {
+            left[i] = Math.max(left[i-1], height[i]);
+        }
+        for(int i=n-2; i>=0; i--) {
+            right[i] = Math.max(right[i+1], height[i]);
+        }
+        int result = 0;
+        for(int i=1; i<n-1; i++) {
+            int m = Math.min(left[i], right[i]);
+            result += m - height[i];
+        }
+        return result;
+    }
+}
